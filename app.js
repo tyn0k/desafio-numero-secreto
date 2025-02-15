0faf6a32e770106291bc9101b374dc113a8fd8a0
Christian Paillacán <c.paillacan.gonzalez@gmail.com>
2025-02-15 18:33:42 -0300

    insertar limpieza de caja

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaAmigoSecreto = []
 
+function limpiarCaja() {
+    document.getElementById('amigo').value = '';
+}
+
 function agregarAmigo() {
     let nuevoAmigo = document.getElementById('amigo').value;
-    console.log(typeof(nuevoAmigo))
-    console.log(nuevoAmigo)
+    console.log(typeof(nuevoAmigo));
+    console.log(nuevoAmigo);
     listaAmigoSecreto.push(nuevoAmigo);
-    console.log(listaAmigoSecreto)
-    
+    console.log(listaAmigoSecreto);
+    limpiarCaja();
 }
