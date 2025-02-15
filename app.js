0c9f6f986bcdb401f6367dfee357610bcd79a285
Christian Paillacán <c.paillacan.gonzalez@gmail.com>
2025-02-15 19:04:17 -0300

    mostrar lista en pagina

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let listaAmigoSecreto = []
+let listaAmigoSecreto = [];
 
 function limpiarCaja() {
     document.getElementById('amigo').value = '';
 }
 
+function mostrarListaEnPagina() {
+    //obtener elemento ul donde se muestra la lista
+    let listaAmigos = document.getElementById('listaAmigos');
+    // limpiar el contenido actual
+    listaAmigos.innerHTML = '';
+
+    // recorrer lista actual de amigos y agregar un ul por cada uno
+    for (let i = 0; i < listaAmigoSecreto.length; i++) {
+        let amigo = listaAmigoSecreto[i]; // obtener el nombre
+        listaAmigos.innerHTML += `<li>${amigo}</li>`; // agregar el nombre como li
+    }
+}
+
 function agregarAmigo() {
-    let nuevoAmigo = document.getElementById('amigo').value;
+    // Obtener el valor del input
+    let nuevoAmigo = document.getElementById('amigo').value.trim();
+    // verificar que el nombre no sea vacío
+    if (nuevoAmigo === ""){
+        alert("Por favor, escribe un nombre");
+        return;
+    }
     console.log(typeof(nuevoAmigo));
     console.log(nuevoAmigo);
+    // agregar el nombre a la lista
+
     listaAmigoSecreto.push(nuevoAmigo);
     console.log(listaAmigoSecreto);
+
+    // limpiar input
     limpiarCaja();
+
+    // mostrar la lista en la página
+    mostrarListaEnPagina();
+
 }
